16d9a4aad3e152134b6d9be92cc58670cd3e024e
Max Malynowsky <maxmalynowsky@gmail.com>
2019-09-17 16:03:35 -0600

    separate routes from index

--- src/index.js
+++ src/index.js
@@ -5,33 +5,30 @@ import http from 'http';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 
-import server1 from './server-1';
+import routes from './routes';
 
 const app = express();
 const httpServer = http.createServer(app);
 
-const applyMiddleware = (server, path) => {
-  server.applyMiddleware({ app, path });
-  server.installSubscriptionHandlers(httpServer);
-};
-
-const connectDb = () =>
-  mongoose.connect(process.env.DATABASE_URL, {
-    auth: { authSource: 'admin' },
-    user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
-
 app.use(cors());
 app.use(morgan('dev'));
 
-applyMiddleware(server1, '/server-1');
+mongoose.connect(process.env.DATABASE_URL, {
+  auth: { authSource: 'admin' },
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-connectDb().then(() => {
+for (const [server, path] of routes) {
+  server.applyMiddleware({ app, path });
+  server.installSubscriptionHandlers(httpServer);
+}
+
+mongoose.connection.once('open', () => {
   httpServer.listen({ port: 4000 }, () => {
     console.log(`Apollo Server on http://localhost:4000`);
   });
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import server1 from './server-1';
+
+const routes = [[server1, '/server-1']];
+
+export default routes;
