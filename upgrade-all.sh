16396a7eb98d889e9af713619fa46025a0c950eb
Max Malynowsky <maxmalynowsky@gmail.com>
2019-10-01 17:43:12 -0600

    add async support to babel

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 yarn add \
+  @babel/runtime \
   apollo-server-express \
   apollo-server-plugin-response-cache \
   compression \
@@ -17,5 +18,6 @@ yarn add --dev \
   @babel/cli \
   @babel/core \
   @babel/node \
+  @babel/plugin-transform-runtime \
   @babel/preset-env \
   nodemon
