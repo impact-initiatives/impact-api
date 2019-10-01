0ddb4ef4ba9321b96abe2fc0c0f17e569a30b14e
Max Malynowsky <maxmalynowsky@gmail.com>
2019-10-01 16:54:56 -0600

    remove example api

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,5 @@
-import server1 from './server-1';
 import ssdWashIm from './ssd-wash-im';
 
-const routes = [
-  // [server1.server, '/server-1', server1.auth],
-  [ssdWashIm.server, '/ssd-wash-im', ssdWashIm.auth],
-];
+const routes = [[ssdWashIm.server, '/ssd-wash-im', ssdWashIm.auth]];
 
 export default routes;
