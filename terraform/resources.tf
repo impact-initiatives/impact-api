13a08e1d3dd17af4f6022d9ddd3ad85fba2a9758
Max Malynowsky <maxmalynowsky@gmail.com>
2019-10-13 22:50:05 -0600

    increase instance size and swap

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -3,7 +3,7 @@ resource "exoscale_compute" "impact_api_1" {
   display_name    = "impact-api-1"
   template        = "Linux CoreOS 2135 64-bit"
   zone            = "ch-gva-2"
-  size            = "Micro"
+  size            = "Tiny"
   disk_size       = 10
   key_pair        = "exoscale-default"
   security_groups = ["impact-api"]
