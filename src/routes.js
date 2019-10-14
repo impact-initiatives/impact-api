49111037ea5f45af832cd2bd8a3b62ccfad4a2dd
Max Malynowsky <maxmalynowsky@gmail.com>
2019-10-14 12:09:38 -0600

    add new project ot routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import ssdWashIm from './ssd-wash-im';
+import ngaTpm from './nga-tpm';
 
-const routes = [[ssdWashIm.server, '/ssd-wash-im', ssdWashIm.auth]];
+const routes = [
+  [ssdWashIm.server, '/ssd-wash-im', ssdWashIm.auth],
+  [ngaTpm.server, '/nga-tpm', ngaTpm.auth],
+];
 
 export default routes;
