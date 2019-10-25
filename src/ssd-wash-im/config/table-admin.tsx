703367ae1ffe0291e31617b92763eb3e1d4daa43
Max Malynowsky <maxmalynowsky@gmail.com>
2019-10-25 17:49:55 -0600

    update admin table headers

--- src/ssd-wash-im/config/table-admin.tsx
+++ src/ssd-wash-im/config/table-admin.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+const dateOptions = { year: 'numeric', month: 'short', timeZone: 'UTC' };
+const updateOptions = { year: 'numeric', month: 'short', day: '2-digit' };
+
 export const tableHeader = [
   'Name',
+  'Date',
   'Organization',
   'Document Type',
   'File Type',
-  'Created',
   'Updated',
 ];
 
@@ -19,25 +22,11 @@ export const tableBody = (data: LabeledData[]) =>
         </Link>
       </td>
       <td>
-        {d.organizations.map(
-          (org, i) => (i ? ', ' : '') + org.value.split(' | ')[0],
-        )}
+        {new Date(d.endDate + '-01').toLocaleString('en-GB', dateOptions)}
       </td>
+      <td>{d.organizations.map((org, i) => (i ? ', ' : '') + org.label)}</td>
       <td>{d.documentType.label}</td>
       <td>{d.fileType.label}</td>
-      <td>
-        {new Date(d.createdAt).toLocaleString('en-GB', {
-          year: 'numeric',
-          month: 'short',
-          day: '2-digit',
-        })}
-      </td>
-      <td>
-        {new Date(d.updatedAt).toLocaleString('en-GB', {
-          year: 'numeric',
-          month: 'short',
-          day: '2-digit',
-        })}
-      </td>
+      <td>{new Date(d.updatedAt).toLocaleString('en-GB', updateOptions)}</td>
     </tr>
   ));
