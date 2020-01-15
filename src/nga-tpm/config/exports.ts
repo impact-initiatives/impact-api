08ad24060fb7770098c155dedfba4a1b428ad372
Max Malynowsky <maxmalynowsky@gmail.com>
2020-01-15 12:43:58 +0100

    add new NGA TPM configuration files

--- src/nga-tpm/config/exports.ts
+++ src/nga-tpm/config/exports.ts
@@ -14,6 +14,34 @@ const exports: Exports = {
   },
   maps: [
     {
+      title: 'All TPM Data',
+      description: 'Complete Light Touch Monitoring Dataset.',
+      img: 'dfid.png',
+      url:
+        'https://public.tableau.com/views/IMPACT_Light_Touch_Monitoring/LTMDashboard?:showVizHome=no',
+    },
+    {
+      title: 'IRC TPM Data',
+      description: 'LTM Dashboard for IRC data only.',
+      img: 'irc.png',
+      url:
+        'https://public.tableau.com/views/IMPACT_Light_Touch_Monitoring_IRC/LTMDashboard?:showVizHome=no',
+    },
+    {
+      title: 'UNICEF TPM Data',
+      description: 'LTM Dashboard for UNICEF data only.',
+      img: 'unicef.png',
+      url:
+        'https://public.tableau.com/views/IMPACT_Light_Touch_Monitoring_UNICEF/LTMDashboard?:showVizHome=no',
+    },
+    {
+      title: 'WFP TPM Data',
+      description: 'LTM Dashboard for WFP data only.',
+      img: 'wfp.png',
+      url:
+        'https://public.tableau.com/views/IMPACT_Light_Touch_Monitoring_WFP/LTMDashboard?:showVizHome=no',
+    },
+    {
       title: 'Reference Atlas',
       description: 'Reference atlas showing location of key infrastructure.',
       img: 'map-1.png',
